b0e3af9b2497827bd2cca3a8726e62b1a3bef5bf
reuben_okotie <99736344+r-okotie179@users.noreply.github.com>
2026-06-22 10:15:45 +0100

    Update analytic-eyam-model.py

--- science by simulation/chapter 2: the epidemiology of eyam/analytic-eyam-model.py
+++ science by simulation/chapter 2: the epidemiology of eyam/analytic-eyam-model.py
@@ -1,12 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# This model will use analytic integration to find the values for alpha and beta then perform linear regression
+# This model will use analytic integration to find the values for alpha and beta
 I = [14.5, 22, 29, 21, 8, 8, 0]
 S = [235, 201, 153.5, 121, 108, 97, 83]
 
-x_val = np.array([np.log(S/S[0]) for S in S])
-y_val = np.array([(I + S - I[0] - S[0]) for I,S in I,S])
+x_val = np.array([np.log(s/S[0]) for s in S])
+y_val = []
+for i in range(len(I)):
+    if len(I) != len(S):
+        raise ValueError
+    val = I[i] + S[i] - I[0] - S[0]
+    y_val.append(val)
+y_val = np.array(y_val)
+#print(x_val, y_val)
 
-print(x_val, y_val)
+# Performing linear regression to find the value of the gradient (by reading the appendix)
+## https://stats.stackexchange.com/questions/631540/from-a-theoretical-standpoint-why-is-linear-regression-useful
+## Note to why looking into linear regression is useful for further progress with prediction.
+
+
+## for this model, alpha is assumed to be 2.87 as I will not dwell on the adjacent linear regression 
+
+# Iterate different values 
+
+plt.scatter(x_val, y_val)
+plt.show()
+
 # From finding the values, I can now to the Euler Method of discrete integration to find the final values
